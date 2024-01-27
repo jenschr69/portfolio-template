398aa4f20c6b17583102bfbd8a564a2f01063375
Jens Chr. Christensen <jcc@jenschrchristensen.com>
2024-01-27 21:25:01 +0100

    Adding instructions to what to put in js file. Adding include tags for JS

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+// Add script for inserting page id
+
+// Add script for inserting global navigation
+
+// Assign classes for navigation to mark which (sub) page the user is on
+// Adding aria classes for this
